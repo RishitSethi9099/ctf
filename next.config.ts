3146e85b7a5497f25fa3a7e102602fcbe2c7bf14
Rishit Sethi <101106223+RishitSethi9099@users.noreply.github.com>
2026-01-31 00:48:08 +0530

    removed:allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', '172.25.144.1', '172.20.10.2'],
+  // allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', '172.25.144.1', '172.20.10.2'],
 };
 
 export default nextConfig;
